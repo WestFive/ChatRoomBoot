f36cbea1d792563bbdbf2b05da3f1d40c10ae312
dubersfeld <dominique42ubersfeld@gmail.com>
2017-07-29 17:48:24 +0200

    Hardcoding removed

--- src/main/java/com/dub/spring/config/WebSocketConfig.java
+++ src/main/java/com/dub/spring/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.dub.spring.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
@@ -11,6 +12,9 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
  
+	@Value("myapp.allowed.origins")
+	private String allowedOrigins;
+	
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
@@ -18,8 +22,8 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
     }
  
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-         registry.addEndpoint("/chat").setAllowedOrigins("http://www.dominique-ubersfeld.com:8080").withSockJS();
+    public void registerStompEndpoints(StompEndpointRegistry registry) { 
+    	registry.addEndpoint("/chat").setAllowedOrigins(allowedOrigins).withSockJS();
     }
     
 }
